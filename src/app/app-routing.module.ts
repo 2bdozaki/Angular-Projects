29ea2c8c728e48a4b087a41dbde23cfaace245c8
2bdozaki <2bdozaki@gmail.com>
2022-07-14 11:17:34 +0200

    first Edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { Error404Component } from './error404/error404.component';
+import { HomeComponent } from './home/home.component';
+import { InformationComponent } from './information/information.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:"", redirectTo:"home",pathMatch:'full'},
+  {path:"home", component:HomeComponent},
+  {path:"nav-bar", component: NavBarComponent},
+  {path:"portfolio", component:PortfolioComponent},
+  {path:"about", component:AboutComponent},
+  {path:"information",component:InformationComponent},
+  {path:"contact-me",component:ContactMeComponent},
+  {path:"**", component:Error404Component},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { InformationComponent } from './information/information.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    PortfolioComponent,
+    AboutComponent,
+    ContactMeComponent,
+    InformationComponent,
+    FooterComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
